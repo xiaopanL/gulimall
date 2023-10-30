ee41a292ed9bfdfd3d9d7b3be8c88bd55c509f64
xiaopang <xiopang@li.com>
2023-10-30 20:12:23 +0800

    项目初识结构创建

---
+++ gulimall-coupon/src/test/java/com/li/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.li.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
